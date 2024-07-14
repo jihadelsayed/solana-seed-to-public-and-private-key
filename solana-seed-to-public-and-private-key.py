9e92957ee6ea1a9a8c33c7e0d0856b01166584ff
Jihad Sayed <david.pales1@gmail.com>
2024-07-14 14:22:09 +0200

    Update solana-seed-to-public-and-private-key.py

--- solana-seed-to-public-and-private-key.py
+++ solana-seed-to-public-and-private-key.py
@@ -1,8 +1,20 @@
+import os
 import solana
 from solders.keypair import Keypair
 from solders.pubkey import Pubkey
 import json
-
+os.system('color')
+def copyRight():
+        print('\033[95m' + """"
+            ____   __ __       ____  ____  __ __   ____  ___          ___  _           _____  ____  __ __    ___  ___   
+            |    \ |  |  |     |    ||    ||  |  | /    ||   \        /  _]| |         / ___/ /    ||  |  |  /  _]|   \  
+            |  o  )|  |  |     |__  | |  | |  |  ||  o  ||    \      /  [_ | |        (   \_ |  o  ||  |  | /  [_ |    \ 
+            |     ||  ~  |     __|  | |  | |  _  ||     ||  D  |    |    _]| |___      \__  ||     ||  ~  ||    _]|  D  |
+            |  O  ||___, |    /  |  | |  | |  |  ||  _  ||     |    |   [_ |     |     /  \ ||  _  ||___, ||   [_ |     |
+            |     ||     |    \  `  | |  | |  |  ||  |  ||     |    |     ||     |     \    ||  |  ||     ||     ||     |
+            |_____||____/      \____||____||__|__||__|__||_____|    |_____||_____|      \___||__|__||____/ |_____||_____|
+            look at www.neetechs.com for more script
+            """ + '\033[0m')
 # Function to get the public key from a Keypair
 def get_public_key_from_keypair():
     keypair = Keypair()
